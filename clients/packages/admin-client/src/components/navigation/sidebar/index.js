13bea3dd7b6321b427611f0eff506666ea0f19b4
Igor Santos <igr.exe@gmail.com>
2022-01-17 13:43:50 -0300

    chore(admin): fetch mobilizations by hasura api

--- clients/packages/admin-client/src/components/navigation/sidebar/index.js
+++ clients/packages/admin-client/src/components/navigation/sidebar/index.js
@@ -1,9 +1,16 @@
-import mapStateToProps from './map-state-to-props'
+import Cookies from 'js-cookie';
 import Sidebar from './sidebar'
 import SidebarContainer from './container'
 
 export default {
   Sidebar,
   SidebarContainer,
-  getSidebarProps: mapStateToProps
+  getSidebarProps: () => {
+    return {
+      loading: false,
+      user: { id: 2,  },
+      mobilization: undefined,
+      community: JSON.parse(Cookies.get('community'))
+    }
+  }
 }
--- clients/packages/admin-client/src/pages/admin/mobilizations/list/page.connected.js
+++ clients/packages/admin-client/src/pages/admin/mobilizations/list/page.connected.js
@@ -1,28 +1,78 @@
-//
-// @route /mobilizations
-//
-import { connect } from 'react-redux'
+import React, { useContext } from 'react';
+import { Context as SessionContext, useQuery, gql } from 'bonde-core-tools';
 import qs from 'query-string'
+import { connect } from 'react-redux'
 import MobSelectors from 'mobrender/redux/selectors'
 import * as MobActions from 'mobrender/redux/action-creators'
+import Page from './page';
+import { useEffect } from 'react';
 
-import Page from './page'
+const FETCH_MOBILIZATIONS = gql`
+  query ($community_id: Int!) {
+    mobilizations(where: { community_id: { _eq: $community_id } }) {
+      id
+      name
+      goal
+      facebook_share_image
+      custom_domain
+      slug
+      status
+      created_at
+      updated_at
+      community {
+        id
+        name
+        city
+      }
+    }
+  }
+`;
+
+const MobilizationList = ({
+  location,
+  history,
+  fetchMobilizations,
+  select,
+  toggleMenu,
+  changeStatus,
+  menuActiveIndex
+}) => {
+  const { community } = useContext(SessionContext);
+  const { data, loading, error } = useQuery(FETCH_MOBILIZATIONS, { variables: { community_id: community.id } });
+
+  useEffect(() => {
+    if (data) fetchMobilizations(data.mobilizations);
+  }, [data, fetchMobilizations])
+
+  if (error) return <p>Failed fetch mobilizations</p>;
+
+  const query = qs.parse(location.search);
+
+  return loading
+    ? <p>'Carregando mobilizações...'</p>
+    : (
+    <Page
+      history={history}
+      mobilizations={data.mobilizations}
+      menuActiveIndex={menuActiveIndex}
+      location={{ ...location, query }}
+      select={select}
+      toggleMenu={toggleMenu}
+      changeStatus={changeStatus}
+    />
+  )
+}
 
 const mapStateToProps = (state, props) => {
   const selectors = MobSelectors(state, props)
-  const { location: { search } } = props
-  const query = qs.parse(search)
   return {
-    mobilizations: selectors.getMobilizations(query),
     menuActiveIndex: selectors.getMobilizationMenuActive(),
-    location: {
-      ...props.location,
-      query
-    }
+    ...props
   }
 }
 
 const mapActionsToProps = (dispatch) => ({
+  fetchMobilizations: (mobilizations) => dispatch({ type: 'FETCH_MOBILIZATIONS_SUCCESS', payload: mobilizations }),
   select: (...args) => dispatch(MobActions.selectMobilization(...args)),
   toggleMenu: (...args) => dispatch(MobActions.toggleMobilizationMenu(...args)),
   changeStatus: (mob) => {
@@ -33,4 +83,4 @@ const mapActionsToProps = (dispatch) => ({
   }
 })
 
-export default connect(mapStateToProps, mapActionsToProps)(Page)
+export default connect(mapStateToProps, mapActionsToProps)(MobilizationList)

231d9861f1a2885aed5aa56ae09d9f162755772e
Igor Santos <igr.exe@gmail.com>
2022-03-16 16:53:11 -0300

    chore(admin): remove bot unused pages

--- clients/packages/admin-client/src/pages/admin/container.js
+++ clients/packages/admin-client/src/pages/admin/container.js
@@ -2,18 +2,12 @@ import React from 'react'
 import { Switch } from 'react-router-dom'
 
 // Routes
-import BetaBotPage from './bot'
 import Sidebar from './sidebar'
 import PrivateRoute from './private-route'
 
 const Logged = () => {
   return (
     <Switch>
-      <PrivateRoute
-        exact
-        path='/bot'
-        component={BetaBotPage}
-      />
       <PrivateRoute
         path='/'
         component={Sidebar}
